d4c0fa82761fd5ea7ce7bd732a25c905194a04c4
Ilera22 <ilera.none@gmail.com>
2023-07-27 17:14:47 -0500

    Modificacion de NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,72 +1,77 @@
 <template>
-    <div id="app">
-      <v-layout row justify-center>
-        <v-responsive v-if="$vuetify.breakpoint.smAndDown">
-          <v-app-bar app color="red">
-            <v-app-bar-title>Mobile</v-app-bar-title>
-            <v-spacer></v-spacer>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          </v-app-bar>
-        </v-responsive>
-  
-        <v-responsive v-else>
-          <v-app-bar app dark color="blue-grey darken-1">
-            <v-app-bar-title>Desktop</v-app-bar-title>
-            <v-spacer></v-spacer>
-            <v-app-bar-items>
-              <v-btn
-                variant="plain"
+  <div id="app">
+    <v-layout row justify-center>
+      <v-responsive v-if="$vuetify.breakpoint.smAndDown">
+        <v-app-bar app color="red">
+          <v-app-bar-nav-icon @click="menuOpen = !menuOpen"></v-app-bar-nav-icon>
+          <v-menu v-model="menuOpen" offset-y>
+            <v-list>
+              <v-list-item
                 v-for="item in nav"
                 :key="item.icon"
                 :to="item.to"
                 :title="item.title"
-                >{{ item.text }}</v-btn
+                @click="menuOpen = false"
               >
-            </v-app-bar-items>
-          </v-app-bar>
-        </v-responsive>
-      </v-layout>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "NavBar",
-    data() {
-      return {
-        dialog: false,
-        nav: [
-          {
-            icon: "home",
-            text: "Home",
-            title: "Back to Home page",
-            active: true,
-            to: "/",
-          },
-          {
-            icon: "info",
-            text: "About",
-            title: "About this demo",
-            active: false,
-            to: "/about",
-          },
-          {
-            icon: "assignment_turned_in",
-            text: "Todos",
-            title: "Some stuff that needs doing",
-            active: false,
-            to: "/todos",
-          },
-          {
-            icon: "email",
-            text: "Contact",
-            title: "Our Contact info",
-            active: false,
-            to: "/contact",
-          },
-        ],
-      };
-    },
-  };
-  </script>
-  
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-app-bar>
+      </v-responsive>
+
+      <v-responsive v-else>
+        <v-app-bar app dark color="blue-grey darken-1">
+          <v-btn>Desktop</v-btn>
+          <v-spacer></v-spacer>
+          <v-app-bar-items>
+            <v-btn
+              variant="plain"
+              v-for="item in nav"
+              :key="item.icon"
+              :to="item.to"
+              :title="item.title"
+            >
+              {{ item.text }}
+            </v-btn>
+          </v-app-bar-items>
+        </v-app-bar>
+      </v-responsive>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      dialog: false,
+      menuOpen: false, // Agregamos esta propiedad para controlar el estado del menú
+      nav: [
+        {
+          icon: "info",
+          text: "About",
+          title: "About this demo",
+          active: false,
+          to: "/about",
+        },
+        {
+          icon: "assignment_turned_in",
+          text: "Todos",
+          title: "Some stuff that needs doing",
+          active: false,
+          to: "/pages/GhostVue",
+        },
+        {
+          icon: "email",
+          text: "Contact",
+          title: "Our Contact info",
+          active: false,
+          to: "/contact",
+        },
+      ],
+    };
+  },
+};
+</script>
