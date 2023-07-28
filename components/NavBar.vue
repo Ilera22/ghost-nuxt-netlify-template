bd5c378a0dfd9970f8079f6ba4e0cee70619a051
Ilera22 <ilera.none@gmail.com>
2023-07-27 22:42:03 -0500

    regreso

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,80 +1,45 @@
 <template>
-  <div id="app">
-    <v-layout row justify-center>
-      <v-responsive v-if="$vuetify.breakpoint.smAndDown">
-        <v-app-bar app color="deep-purple accent-3">
-          <v-btn plain @click="goToIndex">Gremio KTG</v-btn>
-          <v-spacer></v-spacer>
-          <v-app-bar-nav-icon @click="menuOpen = !menuOpen"></v-app-bar-nav-icon>
-          <v-menu v-model="menuOpen" offset-y>
-            <v-list>
-              <v-list-item 
-
-                v-for="item in nav"
-                :key="item.icon"
-                :to="item.to"
-                :title="item.title"
-                @click="menuOpen = false"
-              >
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-app-bar>
-      </v-responsive>
-
-      <v-responsive v-else>
-        <v-app-bar app dark color="deep-purple accent-3">
-          <v-btn plain @click="goToIndex">Gremio KTG</v-btn>
-          <v-spacer></v-spacer>
-          <v-app-bar-items>
-            <v-btn
-              plain
-              v-for="item in nav"
-              :key="item.icon"
-              :to="item.to"
-              :title="item.title"
-            >
-              {{ item.text }}
-            </v-btn>
-          </v-app-bar-items>
-        </v-app-bar>
-      </v-responsive>
-    </v-layout>
-  </div>
+  <v-app dark>
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: "NavBar",
-  data() {
-    return {
-      dialog: false,
-      menuOpen: false, // Agregamos esta propiedad para controlar el estado del menú
-      nav: [
-        {
-          icon: "info",
-          text: "About",
-          title: "About this demo",
-          active: false,
-          to: "/about",
-        },
-        {
-          icon: "assignment_turned_in",
-          text: "Todos",
-          title: "Some stuff that needs doing",
-          active: false,
-          to: "/GhostTest",
-        },
-        {
-          icon: "email",
-          text: "Test",
-          title: "Our Contact info",
-          active: false,
-          to: "/Test",
-        },
-      ],
-    };
+  name: 'EmptyLayout',
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
   },
-};
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
 </script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
