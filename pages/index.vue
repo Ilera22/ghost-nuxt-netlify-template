1175de7db220ef7ecf6f3706eacab3f4ea29b8ad
Ilera22 <ilera.none@gmail.com>
2023-07-27 22:30:30 -0500

    modificacion mayor

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
           justify="center"
         >
           <div class="text-h2">
-            {{ slide }} Slide
+            {{ slide }}
           </div>
         </v-row>
       </v-sheet>
@@ -29,24 +29,35 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
-  }
+import { getPosts } from '../api/ghost';
+
+export default {
+  data() {
+    return {
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [],
+    };
+  },
+  async mounted() {
+    try {
+      // Obtiene los últimos 5 posts desde el API de Ghost
+      const posts = await getPosts(5);
+
+      // Actualiza el array de slides con los títulos de los posts
+      this.slides = posts.map((post) => post.title);
+    } catch (error) {
+      console.error('Error al obtener los posts:', error);
+    }
+  },
+};
 </script>
+
+<style>
+/* Estilos personalizados para el carrusel si es necesario */
+</style>
